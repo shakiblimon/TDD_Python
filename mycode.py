ab875f7403a6c38903495e5e970a89afcc61625b
Shakib Limon <shakib.limon@circlefintech.com>
2019-02-27 11:58:09 +0600

    Custom non linear number list

--- mycode.py
+++ mycode.py
@@ -1,11 +1,14 @@
 def hello_world():
     return "hello world!"
 
+#   create a number list
 def creat_num_list(length):
     return [x for x in range (length)]
 
+#   custom function
 def custom_func_x(x, const, power):
     return const * (x) ** power
 
+#   Custom non linear number list
 def custom_non_lin_num_list(length , const, power):
     return [custom_func_x(x,const,power) for x in range (length)]
--- mytest.py
+++ mytest.py
@@ -7,12 +7,16 @@ class Myfirsttest(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world(), 'hello world!')
 
+#   test custome number list
     def test_custom_num_list(self):
         self.assertEqual(len(creat_num_list(10)), 10)
 
+#   test cusrome function x
     def test_custom_func_x(self):
         self.assertEqual(custom_func_x(3, 2, 3), 54)
 
-    def tes_custome_non_lin_num_list(self):
+
+#    test custome non linear number list
+    def test_custom_non_lin_num_list(self):
         self.assertEqual(custom_non_lin_num_list(5,2,3)[2],16)
         self.assertEqual(custom_non_lin_num_list(5,2,3)[4],48)
